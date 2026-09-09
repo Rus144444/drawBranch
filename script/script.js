228cebf5bbc7ff303417b82c7e8bda5afa951290
Ruslan Azizov <ruslanazizov@ruslans-macbook-air.home>
2026-09-09 15:41:48 +0200

    add new canvas, learn and practic in canvas

--- script/script.js
+++ script/script.js
@@ -1,10 +1,88 @@
 'use strict';
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('myCanvas-1');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = '#e9dedef1';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // прическа 
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 125, 1.1 * Math.PI, 1.9  * Math.PI, false);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 130, 1.2 * Math.PI, 1.8  * Math.PI, false);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 135, 1.3 * Math.PI, 1.7  * Math.PI, false);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 140, 1.4 * Math.PI, 1.6  * Math.PI, false);
+    ctx.stroke();
+
+    // ухо левое
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2 - 120, canvas.height / 2, 20, 0.5 * Math.PI, 1.5  * Math.PI, false);
+    ctx.stroke();
+
+    // ухо правое
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2 + 120, canvas.height / 2, 20, 1.5 * Math.PI, 2.5  * Math.PI, false);
+    ctx.stroke();
+
+    // круг, морда 
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 120, 0.1 * Math.PI, 2.9  * Math.PI, false);
+    ctx.stroke();
+
+    // нос
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "brown";
+    ctx.arc(canvas.width / 2, canvas.height / 2 + 20, 10, 0.1 * Math.PI, 2.9  * Math.PI, false);
+    ctx.stroke();
+
+    // глаза 
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "blue";
+    ctx.arc(canvas.width / 2 - 50, canvas.height / 2 - 50, 10, 0.1 * Math.PI, 2.9  * Math.PI, false);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "blue";
+    ctx.arc(canvas.width / 2 + 50, canvas.height / 2 - 50, 10, 0.1 * Math.PI, 2.9  * Math.PI, false);
+    ctx.stroke();
+
+
+    // улыбка
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "red";
+    ctx.lineCap = "round";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 95, 0.1 * Math.PI, 0.9  * Math.PI, false);
+    ctx.stroke();
+});
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById('myCanvas-2');
     const ctx = canvas.getContext('2d');
 
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 2;
 
@@ -12,7 +90,7 @@ window.addEventListener('load', () => {
     ctx.lineTo(canvas.width, 0);
     ctx.stroke();
 
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 2;
 
@@ -20,7 +98,7 @@ window.addEventListener('load', () => {
     ctx.lineTo(0, 0);
     ctx.stroke();
 
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 2;
 
@@ -28,15 +106,13 @@ window.addEventListener('load', () => {
     ctx.lineTo(canvas.width / 2, 0 );
     ctx.stroke();
 
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.strokeStyle = 'yellow';
     ctx.lineWidth = 2;
 
     ctx.moveTo(canvas.width, canvas.height / 2);
     ctx.lineTo(0, canvas.height / 2 );
     ctx.stroke();
-
-
 }); 
 
 
@@ -47,7 +123,6 @@ window.addEventListener('load', () => {
     console.log('Canvas initialized:', canvas);
     console.log('2D Context initialized:', ctx);
     drawBranch(ctx, canvas.width / 2, canvas.height, 50, 0);
-
 }); 
 
 function drawBranch(ctx, startX, startY, trunkWidth, level) {
